b0d0bf70c618b5ef443ad88a5e816c247f593457
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-27 22:43:07 -0400

    Se refactorizo la clase Birthay Service

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -1,44 +1,16 @@
-import fs from "fs";
-import path from "path";
-import { Employee } from "./Employee";
-
+import { EmployeesRepository } from "./EmployeesRepository";
+import { GreetingDelivery } from "./GreetingDelivery";
 export class BirthdayService {
-  constructor() {}
-
-  sendGreetings(ourDate, fileName, smtpUrl, smtpPort, transport) {
-    const data = fs.readFileSync(
-      path.resolve(__dirname, `${fileName}`), //`../${fileName}`),
-      "UTF-8"
-    );
-
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    lines.shift();
-    const employees = lines
-      .map((line) => this.createEmployeeFromLine(line))
-      .filter((employee) => employee.isBirthday(ourDate));
-
-    employees.forEach((employee) => {
-      const message = {
-        host: smtpUrl,
-        port: smtpPort,
-        from: "sender@here.com",
-        to: [employee.getEmail()],
-        subject: "Happy Birthday!",
-        text: `Happy Birthday, dear ${employee.getFirstName()}!`,
-      };
-      transport.sendMail(message);
-    });
+  constructor(greetingDelivery, employeesRepository) {
+    this.greetingDelivery = GreetingDelivery;
+    this.employeesRepository = EmployeesRepository;
   }
 
-  createEmployeeFromLine(line) {
-    const employeeData = line.split(", ");
-    const employee = new Employee(
-      employeeData[1],
-      employeeData[0],
-      employeeData[2],
-      employeeData[3]
-    );
-    return employee;
+  sendGreetings(ourDate) {
+    const employees = this.employeesRepository.getEmployeesByBirthDate(ourDate);
+
+    employees.forEach(employee => {
+      this.greetingDelivery.sendGreetingToEmployee(employee);
+    });
   }
 }
