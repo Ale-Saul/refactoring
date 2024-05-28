869b4a84fd46e6f8253083265a9cdcd643f407e6
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-27 22:48:38 -0400

    Se refactorizo la prueba de los mensajes a los cumpleañeros

--- src/birthday/Acceptance.spec.js
+++ src/birthday/Acceptance.spec.js
@@ -1,13 +1,16 @@
 import { OurDate } from "./OurDate";
 import { InMemoryTransport } from "./InMemoryTransport";
 import { BirthdayService } from "./BirthdayService";
+import { FileEmployeeRepository } from "./EmployeesRepository";
+import { SmtpGreetingDelivery } from "./GreetingDelivery";
+
 
 describe("Acceptance", () => {
   const SMTP_PORT = 25;
   const SMTP_URL = "localhost";
   const FILENAME = "employee_data.txt";
   let birthdayService;
-  let transport; // = new InMemoryTransport();
+  let transport; 
 
   beforeEach(() => {
     transport = new InMemoryTransport();
@@ -15,22 +18,29 @@ describe("Acceptance", () => {
   });
 
   it("base scenario", () => {
-    birthdayService.sendGreetings(
-      new OurDate("2008/10/08"),
-      "employee_data.txt",
-      SMTP_URL,
-      SMTP_PORT,
-      transport
-    );
+    const SMTP_PORT = 25;
+    const SMTP_URL = "localhost";
+    const FILENAME = "employee_data.txt";
+
+    const transport = nodemailer.createTransport({
+      host: SMTP_URL,
+      port: SMTP_PORT,
+      secure: false,
+      auth: {
+        user: "username",
+        pass: "password",
+      },
+    });
+
+    const messageDelivery = new SmtpGreetingDelivery(SMTP_URL, SMTP_PORT, transport);
+    const employeesRepository = new FileEmployeeRepository(FILENAME);
+    const birthdayService = new BirthdayService(messageDelivery, employeesRepository);
+
+    birthdayService.sendGreetings(new Date("2008/10/08"));
 
     expect(transport.messagesSent.length).toEqual(1);
-    const message = transport.messagesSent[0];
-    expect(message.text).toEqual("Happy Birthday, dear John!");
-    expect(message.subject).toEqual("Happy Birthday!");
-    const tos = message.to;
-    expect(tos.length).toEqual(1);
-    expect(tos[0]).toEqual("john.doe@foobar.com");
   });
+
 
   it("will not send emails when nobodys birthday", () => {
     birthdayService.sendGreetings(
